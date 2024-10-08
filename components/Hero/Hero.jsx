00c889b7cea9fd6f26950350615a338f4b36cf6b
Luxad <l.abromavicius@gmail.com>
2024-10-08 14:23:55 +0300

    fixes

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,56 +1,46 @@
 "use client";
-import React, { useState } from "react";
+
 import TypewriterComponent from "typewriter-effect";
 import HeroCards from "./HeroCards";
-import Intro from "../Intro/Intro";
 
 const Hero = () => {
-  const [showIntro, setShowIntro] = useState(true);
-
-  const handleAnimationEnd = () => {
-    setShowIntro(false);
-  };
-
   return (
     <>
-      {showIntro && <Intro onAnimationEnd={handleAnimationEnd} />}
-      <div className={`${showIntro ? "hidden" : "block"}`}>
-        <div className="relative flex flex-col justify-center items-center dark:bg-slate-800 bg-contain bg-center bg-no-repeat text-center rounded-b h-dvh text-white transform transition-transform duration-500">
-          {/* Video Background */}
-          <video
-            className="absolute inset-0 w-full h-full object-cover"
-            src="/assets/videos/hero.mp4"
-            autoPlay
-            muted
-            loop
-          ></video>
+      <div className="relative flex flex-col justify-center items-center dark:bg-slate-800 bg-contain bg-center bg-no-repeat text-center rounded-b h-dvh text-white transform transition-transform duration-500">
+        {/* Video Background */}
+        <video
+          className="absolute inset-0 w-full h-full object-cover"
+          src="/assets/videos/hero.mp4"
+          autoPlay
+          muted
+          loop
+        ></video>
 
-          {/* Background Overlay */}
-          <div className="absolute inset-0 dark:bg-black opacity-60 backdrop-blur-sm"></div>
+        {/* Background Overlay */}
+        <div className="absolute inset-0 dark:bg-black opacity-60 backdrop-blur-sm"></div>
 
-          {/* Content */}
-          <div className="relative z-10">
-            <span className="flex justify-center text-4xl md:text-6xl font-bold mb-4 overflow-hidden whitespace-nowrap">
-              <span className="text-red-600">RESET</span>AS.LT
-            </span>
+        {/* Content */}
+        <div className="relative z-10">
+          <span className="flex justify-center text-4xl md:text-6xl font-bold mb-4 overflow-hidden whitespace-nowrap">
+            <span className="text-red-600">RESET</span>AS.LT
+          </span>
 
-            <span className="text-lg md:text-2xl mb-8 text-neutral-300 w-80 md:w-full overflow-hidden">
-              <TypewriterComponent
-                options={{
-                  strings: ["Visos IT paslaugos vienoje vietoje"],
-                  autoStart: true,
-                  loop: true,
-                  delay: 170,
-                }}
-              />
-            </span>
+          <span className="text-lg md:text-2xl mb-8 text-neutral-300 w-80 md:w-full overflow-hidden">
+            <TypewriterComponent
+              options={{
+                strings: ["Visos IT paslaugos vienoje vietoje"],
+                autoStart: true,
+                loop: true,
+                delay: 125,
+              }}
+            />
+          </span>
 
-            <button className="bg-red-600 animate-pulse hover:bg-red-700 text-white font-bold py-2 px-4 mt-1 rounded transition ease-in">
-              PAGALBA
-            </button>
+          <button className="bg-red-600 animate-pulse hover:bg-red-700 text-white font-bold py-2 px-4 mt-1 rounded transition ease-in">
+            PAGALBA
+          </button>
 
-            <HeroCards showIntro={showIntro} />
-          </div>
+          <HeroCards />
         </div>
       </div>
     </>
--- components/Hero/HeroCards.jsx
+++ components/Hero/HeroCards.jsx
@@ -26,7 +26,7 @@ const HeroCards = ({ showIntro }) => {
           key={index}
           className="card mt-2"
           initial={{ opacity: 0 }}
-          animate={{ opacity: showIntro ? 0 : 1 }}
+          animate={{ opacity: [0, 1] }}
           transition={{ delay: index * 0.75, duration: 1 }}
         >
           <Icon size={35} />
